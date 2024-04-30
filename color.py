7076bbd310cbc682a7dfca50c35eda4e4fa9c676
NobodySpecial <theheadlessserpentsec@protonmail.com>
2024-04-30 10:44:55 +0000

    Optimized output

--- color.py
+++ color.py
@@ -6,25 +6,74 @@ from qui.clipboard import pyinotify, qubesadmin, NotificationApp, DATA, Gtk, Gdk
 
 from html import escape
 
-if len(argv) != 2:
+TAG_START = lambda color: "<span data-mx-color='%s' style='color: %s;'>" %(color, color)
+TAG_END = lambda: "</span>"
+
+if len(argv) not in [1, 2]:
 	print("Usage: %s <color>" %(argv[0]), file=stderr)
 	raise SystemExit
 
+class ColoredChar(object):
+	def __init__(self, color, char):
+		self.char = char
+		self.html = escape(char)
+		self.color = color
+	def __str__(self):
+		return self.html
+
+class ColoredString(object):
+	def __init__(self):
+		self.chars = []
+	def __add__(self, char):
+		if isinstance(char, ColoredChar):
+			self.chars += [char]
+		elif isinstance(char, (list, tuple)):
+			for c in char:
+				self.__add__(c)
+		else:
+			raise TypeError("char must be of type ColoredChar")
+
+		return self
+	def __str__(self):
+		if not self.chars:
+			return ""
+
+		ret = ""
+		if self.chars[0].color != None:
+			ret += TAG_START(self.chars[0].color)
+
+		ret += str(self.chars[0])
+		last_color = self.chars[0].color
+
+		for char in self.chars[1:]:
+			color = char.color
+			if color != last_color:
+				if last_color != None:
+					ret += TAG_END()
+				if color != None:
+					ret += TAG_START(color)
+				last_color = color
+			ret += str(char)
+		if last_color != None:
+			ret += TAG_END()
+
+		return ret
+
 def colorify_trans3(char, ix, length):
 	colors = ["#5BCEFA", "#F5A9B8", "#FFFFFF"]
-	return "<span data-mx-color='%s' style='color: %s;'>%s</span>" %(colors[ix * len(colors) // length], colors[ix * len(colors) // length], escape(char))
+	return ColoredChar(colors[ix * len(colors) // length], char)
 def colorify_trans5(char, ix, length):
 	colors = ["#5BCEFA", "#F5A9B8", "#FFFFFF", "#F5A9B8", "#5BCEFA"]
-	return "<span data-mx-color='%s' style='color: %s;'>%s</span>" %(colors[ix * len(colors) // length], colors[ix * len(colors) // length], escape(char))
+	return ColoredChar(colors[ix * len(colors) // length], char)
 def colorify_nonbinary(char, ix, length):
 	colors = ["#FCF434", "#FFFFFF", "#9C59D1", "#2C2C2C"]
-	return "<span data-mx-color='%s' style='color: %s;'>%s</span>" %(colors[ix * len(colors) // length], colors[ix * len(colors) // length], escape(char))
+	return ColoredChar(colors[ix * len(colors) // length], char)
 def colorify_rgb(char, ix, length, hex):
-	return "<span data-mx-color='%s' style='color: %s;'>%s</span>" %(hex, hex, escape(char))
+	return ColoredChar(hex, char)
 
 colors = {
-		"default": lambda char, ix, length: "<span data-mx-color='#eeaaff' style='color: #eeaaff;'>%s</span>" %(escape(char)),
-		"none": lambda char, ix, length: char,
+		"default": lambda char, ix, length: ColoredChar("#eeaaff", char),
+		"none": lambda char, ix, length: ColoredChar(None, char),
 		"trans": colorify_trans5,
 		"trans3": colorify_trans3,
 		"trans5": colorify_trans5,
@@ -34,7 +83,10 @@ colors = {
 
 colorify = colors["default"]
 
-color = argv[1]
+color = ""
+if len(argv) >= 2:
+	color = argv[1]
+
 if color not in ["default", ""] and color[0] not in ["#"]:
 	colorify = colors[color]
 if len(color) > 0 and color[0] in ["#"]:
@@ -50,12 +102,12 @@ text = str(clipboard.wait_for_text()) # Save dom0's old clipboard
 with open(DATA, 'r', encoding='utf-8') as contents:
 	global_text = contents.read()
 
-	colored = ""
+	colored = ColoredString()
 	global_text_len = len(global_text)
 	for ix, char in enumerate(global_text):
 		colored += colorify(char, ix, global_text_len)
 
-clipboard.set_text(colored, -1)
+clipboard.set_text(str(colored), -1)
 gtk_app.copy_dom0_clipboard()
 
 if text != None:
